d56a10ac4b26d19096853eb49c29a71a07cf8514
neillbarrett <barrettneill@gmail.com>
2024-01-07 18:46:59 -0800

    Completed Step 7

--- app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WaterCounter.kt
@@ -1,6 +1,5 @@
 package com.example.basicsstatecodelab
 
-import android.widget.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -15,16 +14,14 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter (modifier: Modifier = Modifier) {
-    Column(modifier =  modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
+    Column(modifier =  Modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(8.dp), enabled = count < 10) {
+
+        Button(onClick = { count++ }, enabled = count < 10) {
             Text("Add one")
         }
     }
